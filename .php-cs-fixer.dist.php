89490185873a25dc923cb689b65e0e51a1f3baef
Jakub Klonowski <67651404+kapitanjaszczur@users.noreply.github.com>
2026-08-24 23:04:32 +0200

    Require strict types in all PHP files
    Added strict_types=1 declaration in files that were missing it.
    PHP Fixer rules were adjusted to enforce it.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,7 +6,9 @@ $finder = (new PhpCsFixer\Finder())
     ->in([__DIR__.'/src', __DIR__.'/tests', __DIR__.'/migrations']);
 
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder);
